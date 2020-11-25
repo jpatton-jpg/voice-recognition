6376ef1cc30f91ee2bfd9dd25356681fcdb67d1d
CrunchyCheetos <joseph.patton@maine.edu>
2020-11-25 12:30:29 -0500

    removed unneeded imports

--- kmeans.py
+++ kmeans.py
@@ -2,9 +2,6 @@
 # Joseph Patton #
 
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import KMeans
 
 
